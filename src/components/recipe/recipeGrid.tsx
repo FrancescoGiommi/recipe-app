9ac52b9de9ec40501c14ab849145d171b5c2a6b4
Francesco <155629335+FrancescoGiommi@users.noreply.github.com>
2026-06-27 01:47:32 +0200

    Add grid in RecipeGrid

--- src/components/recipe/recipeGrid.tsx
+++ src/components/recipe/recipeGrid.tsx
@@ -7,11 +7,11 @@ interface RecipeGridProps {
 
 function RecipeGrid({ recipes }: RecipeGridProps) {
   return (
-    <>
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {recipes.map((recipe) => (
         <RecipeCard key={recipe.id} recipe={recipe} />
       ))}
-    </>
+    </div>
   );
 }
 

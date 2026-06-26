888aa05b6c790c8ede9fbb837d1a4a071311b62b
Francesco <155629335+FrancescoGiommi@users.noreply.github.com>
2026-06-27 01:26:19 +0200

    Add RecipeGrid to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-
-import "./App.css";
+import RecipeGrid from "./components/recipe/recipeGrid";
+import { mockRecipes } from "./data/mockRecipes";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <main className="flex min-h-screen items-center justify-center bg-orange-50">
-        <h1 className="text-5xl font-bold text-orange-600">RecipeHub 🍽️</h1>
+        <RecipeGrid recipes={mockRecipes} />
       </main>
     </>
   );

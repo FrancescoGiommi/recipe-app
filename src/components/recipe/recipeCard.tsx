9fc7f3cba26ca7891e874de86334135ad1ba10fc
Francesco <155629335+FrancescoGiommi@users.noreply.github.com>
2026-06-27 01:43:23 +0200

    Add RecipeCard info

--- src/components/recipe/recipeCard.tsx
+++ src/components/recipe/recipeCard.tsx
@@ -6,9 +6,27 @@ interface RecipeCardProps {
 
 function RecipeCard({ recipe }: RecipeCardProps) {
   return (
-    <article>
-      <h1>{recipe.title}</h1>
-    </article>
+    <>
+      <article className="overflow-hidden  rounded-2xl  bg-white  shadow-md">
+        <img
+          src={recipe.image}
+          alt={recipe.title}
+          className="h-48  w-full  object-cover"
+        />
+
+        <div className="p-4">
+          <h2 className="text-xl font-bold  text-slate-900">{recipe.title}</h2>
+
+          <p className="mt-2  text-sm  text-slate-600">
+            {recipe.readyInMinutes} min - {recipe.servings} porzioni
+          </p>
+
+          <p className="mt-2 text-sm  font-medium  text-orange-600">
+            Difficoltà: {recipe.difficulty}
+          </p>
+        </div>
+      </article>
+    </>
   );
 }
 
